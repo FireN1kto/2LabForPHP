ecbdfb85042f359668f2ac6beb443284e020af71
F1reN1kto <maksimcymbal77@gmail.com>
2025-06-21 21:36:03 +0700

    created autorization and connected the database

--- config/database.php
+++ config/database.php
@@ -1,1 +1,17 @@
 <?php
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'guarantee_repair';
+
+// Подключение к MySQL
+$mysqli = mysqli_connect($host, $user, $password, $database);
+
+// Проверка ошибок подключения
+if (mysqli_connect_errno()) {
+    die("Ошибка подключения к MySQL: " . mysqli_connect_error());
+}
+
+
+mysqli_set_charset($mysqli, 'utf8');
+?>
--- incudes/auth.php
+++ incudes/auth.php
@@ -1,1 +1,24 @@
 <?php
+session_start();
+
+function isClientLoggedIn() {
+    return isset($_SESSION['client_id']);
+}
+
+function isStaffLoggedIn() {
+    return isset($_SESSION['staff_id']);
+}
+
+function requireClientLogin() {
+    if (!isClientLoggedIn()) {
+        header('Location: /client/login.php');
+        exit();
+    }
+}
+
+function requireStaffLogin() {
+    if (!isStaffLoggedIn()) {
+        header('Location: /staff/login.php');
+        exit();
+    }
+}
